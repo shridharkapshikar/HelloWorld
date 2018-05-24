e8d29b864de614ef8fd347913762a784b7f07f82
shridhar <shridhar.kapshikar@hotmail.com>
2018-05-24 07:18:12 -0400

    project code

---
+++ CubeGeneratorWeb/Testing/src/Testing.java
@@ -0,0 +1,13 @@
+import java.util.Calendar;
+
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String calSec=Calendar.SECOND+":"+"00";
+	    System.out.println((calSec));
+        
+	}
+
+}
